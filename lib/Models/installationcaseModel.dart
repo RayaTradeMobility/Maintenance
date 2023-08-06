79b282b6faa3597967ff7d37f85e0f1c068614ae
mhhmdoibrahim <mohamed_oibrahim@rayacorp.com>
2023-08-06 12:19:22 +0300

    update

---
+++ lib/Models/installationcaseModel.dart
@@ -0,0 +1,70 @@
+class InstallationModel {
+  String? code;
+  String? message;
+  List<InstallationCases>? installationCases;
+
+  InstallationModel({this.code, this.message, this.installationCases});
+
+  InstallationModel.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    message = json['message'];
+    if (json['installationCases'] != null) {
+      installationCases = <InstallationCases>[];
+      json['installationCases'].forEach((v) {
+        installationCases!.add(InstallationCases.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['code'] = code;
+    data['message'] = message;
+    if (installationCases != null) {
+      data['installationCases'] =
+          installationCases!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class InstallationCases {
+  String? customerFullName;
+  String? mobileNumber;
+  String? phoneNumber;
+  String? city;
+  String? address;
+  String? symptom;
+  String? model;
+
+  InstallationCases(
+      {this.customerFullName,
+        this.mobileNumber,
+        this.phoneNumber,
+        this.city,
+        this.address,
+        this.symptom,
+        this.model});
+
+  InstallationCases.fromJson(Map<String, dynamic> json) {
+    customerFullName = json['customerFullName'];
+    mobileNumber = json['mobileNumber'];
+    phoneNumber = json['phoneNumber'];
+    city = json['city'];
+    address = json['address'];
+    symptom = json['symptom'];
+    model = json['model'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['customerFullName'] = customerFullName;
+    data['mobileNumber'] = mobileNumber;
+    data['phoneNumber'] = phoneNumber;
+    data['city'] = city;
+    data['address'] = address;
+    data['symptom'] = symptom;
+    data['model'] = model;
+    return data;
+  }
+}
